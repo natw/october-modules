cfe7c8c552209c8989da87572d3aebbb9a907fa3
Nat Williams <nat.williams@gmail.com>
2023-11-11 08:54:54 -0600

    add banana ports

--- src/Random.cpp
+++ src/Random.cpp
@@ -34,19 +34,24 @@ struct Random : Module {
 
   Random() {
     config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
-    configParam(FLUCRATE_PARAM, 0.F, 1.F, 0.F, "");
-    configParam(QUANTSTATES_PARAM, 0.F, 1.F, 0.F, "");
-    configParam(STOREDPROB_PARAM, 0.F, 1.F, 0.F, "");
-    configInput(FLUCRATECV_INPUT, "");
-    configInput(QUANTTRIGGER_INPUT, "");
-    configInput(QUANTSTATESCV_INPUT, "");
-    configInput(STOREDTRIGGER_INPUT, "");
-    configInput(STOREDPROBCV_INPUT, "");
-    configOutput(FLUC_OUTPUT, "");
-    configOutput(QUANTLIN_OUTPUT, "");
-    configOutput(QUANTEXP_OUTPUT, "");
-    configOutput(STOREDEVEN_OUTPUT, "");
-    configOutput(STOREDDIST_OUTPUT, "");
+
+    configInput(FLUCRATECV_INPUT, "FRV Rate CV");
+    configParam(FLUCRATE_PARAM, std::log2(0.05F), std::log2(50.F),
+                std::log2(5.F), "Rate", "Hz", 2.F);
+    configOutput(FLUC_OUTPUT, "FRV");
+
+    configInput(QUANTTRIGGER_INPUT, "QRV Trigger");
+    configInput(QUANTSTATESCV_INPUT, "QRV States CV");
+    configParam(QUANTSTATES_PARAM, 1.F, 6.F, 1.F, "States");
+    getParamQuantity(QUANTSTATES_PARAM)->snapEnabled = true;
+    configOutput(QUANTEXP_OUTPUT, "QRV 2^n");
+    configOutput(QUANTLIN_OUTPUT, "QRV n+1");
+
+    configInput(STOREDTRIGGER_INPUT, "SRV Trigger");
+    configInput(STOREDPROBCV_INPUT, "SRV Probability CV");
+    configParam(STOREDPROB_PARAM, 0.F, 1.F, 0.F, "SRV Probability");
+    configOutput(STOREDEVEN_OUTPUT, "SRV Even Dist");
+    configOutput(STOREDDIST_OUTPUT, "SRV Variable Dist");
 
     // these coefficients are lifted from the Fundamental plugin's Noise module
     const float b[] = {0.00425611, 0.00425611};
@@ -56,19 +61,21 @@ struct Random : Module {
 
   dsp::IIRFilter<2, 2> redFilter;
   float triPhase = 0.F;
-  const float triangleFreq = 100.F;
-  // this is the frequency of the clock for the FRV section's
-  // internal sample & hold
-  const float flucClockFreq = 12.F;
+  const float triangleFreq = 10.F;  // noisy triangle's frequency
   Clock frvClock;
   dsp::RCFilter flucTriFilter;
 
   void process(const ProcessArgs& args) override {
-    float noisyTriangle = generateNoisyTriangle(args);
-    if (frvClock.process(args.sampleTime, flucClockFreq)) {
-      /* flucTriFilter.setCutoffFreq(flucClockFreq / args.sampleRate); */
-      flucTriFilter.setCutoffFreq(1000.F / args.sampleRate);
-      flucTriFilter.process(noisyTriangle);
+    float frvRateCV = inputs[FLUCRATECV_INPUT].getVoltage();
+    float frvRateParam = params[FLUCRATE_PARAM].getValue();
+    float frvClockFreq = dsp::exp2_taylor5(frvRateParam + (frvRateCV / 2.F));
+
+    float triSample = generateNoisyTriangle(args);
+    outputs[QUANTEXP_OUTPUT].setVoltage(triSample);
+    if (frvClock.process(args.sampleTime, frvClockFreq)) {
+      /* DEBUG("FRV Sample: %f", triSample); */
+      flucTriFilter.setCutoffFreq(frvClockFreq * 20 / args.sampleRate);
+      flucTriFilter.process(triSample);
     }
     outputs[FLUC_OUTPUT].setVoltage(flucTriFilter.lowpass());
   }
@@ -79,12 +86,12 @@ struct Random : Module {
   //
   // any constants here were arrived at experimentally
   // to get a nice looking wave
-  auto generateNoisyTriangle(const ProcessArgs& args) -> float {
+  float generateNoisyTriangle(const ProcessArgs& args) {
     float redNoise = redFilter.process(random::normal());
     redNoise *= 25.F;
-    float freq = triangleFreq + redNoise * 100;
+    float freq = triangleFreq + redNoise * 10;
     advanceTrianglePhase(freq, args.sampleTime);
-    float y = 5 * abs((triPhase * 2) - 1);
+    float y = 10 * abs((triPhase * 2) - 1);
     return y;
   }
 
@@ -114,37 +121,37 @@ struct RandomWidget : ModuleWidget {
     addParam(createParamCentered<RoundBlackKnob>(
         mm2px(Vec(34.29, 107.1)), module, Random::STOREDPROB_PARAM));
 
-    addInput(createInputCentered<PJ301MPort>(mm2px(Vec(11.43, 21.42)), module,
-                                             Random::FLUCRATECV_INPUT));
-    addInput(createInputCentered<PJ301MPort>(mm2px(Vec(11.43, 42.84)), module,
-                                             Random::QUANTTRIGGER_INPUT));
-    addInput(createInputCentered<PJ301MPort>(mm2px(Vec(11.43, 64.26)), module,
-                                             Random::QUANTSTATESCV_INPUT));
-    addInput(createInputCentered<PJ301MPort>(mm2px(Vec(11.43, 85.68)), module,
-                                             Random::STOREDTRIGGER_INPUT));
-    addInput(createInputCentered<PJ301MPort>(mm2px(Vec(11.43, 107.1)), module,
-                                             Random::STOREDPROBCV_INPUT));
+    addInput(createInputCentered<BlueBananaPort>(
+        mm2px(Vec(11.43, 21.42)), module, Random::FLUCRATECV_INPUT));
+    addInput(createInputCentered<RedBananaPort>(
+        mm2px(Vec(11.43, 42.84)), module, Random::QUANTTRIGGER_INPUT));
+    addInput(createInputCentered<BlueBananaPort>(
+        mm2px(Vec(11.43, 64.26)), module, Random::QUANTSTATESCV_INPUT));
+    addInput(createInputCentered<RedBananaPort>(
+        mm2px(Vec(11.43, 85.68)), module, Random::STOREDTRIGGER_INPUT));
+    addInput(createInputCentered<BlueBananaPort>(
+        mm2px(Vec(11.43, 107.1)), module, Random::STOREDPROBCV_INPUT));
 
-    addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(79.897, 21.436)),
-                                               module, Random::FLUC_OUTPUT));
-    addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(80.01, 42.84)), module,
-                                               Random::QUANTLIN_OUTPUT));
-    addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(80.01, 64.26)), module,
-                                               Random::QUANTEXP_OUTPUT));
-    addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(80.01, 85.68)), module,
-                                               Random::STOREDEVEN_OUTPUT));
-    addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(80.01, 107.1)), module,
-                                               Random::STOREDDIST_OUTPUT));
+    addOutput(createOutputCentered<BlueBananaPort>(
+        mm2px(Vec(79.897, 21.436)), module, Random::FLUC_OUTPUT));
+    addOutput(createOutputCentered<BlueBananaPort>(
+        mm2px(Vec(80.01, 42.84)), module, Random::QUANTLIN_OUTPUT));
+    addOutput(createOutputCentered<BlueBananaPort>(
+        mm2px(Vec(80.01, 64.26)), module, Random::QUANTEXP_OUTPUT));
+    addOutput(createOutputCentered<BlueBananaPort>(
+        mm2px(Vec(80.01, 85.68)), module, Random::STOREDEVEN_OUTPUT));
+    addOutput(createOutputCentered<BlueBananaPort>(
+        mm2px(Vec(80.01, 107.1)), module, Random::STOREDDIST_OUTPUT));
 
-    addChild(createLightCentered<SmallLight<RedLight>>(
+    addChild(createLightCentered<SmallLight<GreenRedLight>>(
         mm2px(Vec(84.897, 16.42)), module, Random::FLUC_LIGHT));
-    addChild(createLightCentered<SmallLight<RedLight>>(
+    addChild(createLightCentered<SmallLight<GreenRedLight>>(
         mm2px(Vec(85.01, 37.84)), module, Random::QUANTLIN_LIGHT));
-    addChild(createLightCentered<SmallLight<RedLight>>(
+    addChild(createLightCentered<SmallLight<GreenRedLight>>(
         mm2px(Vec(85.01, 59.26)), module, Random::QUANTEXP_LIGHT));
-    addChild(createLightCentered<SmallLight<RedLight>>(
+    addChild(createLightCentered<SmallLight<GreenRedLight>>(
         mm2px(Vec(85.01, 80.68)), module, Random::STOREDEVEN_LIGHT));
-    addChild(createLightCentered<SmallLight<RedLight>>(
+    addChild(createLightCentered<SmallLight<GreenRedLight>>(
         mm2px(Vec(85.01, 102.1)), module, Random::STOREDDIST_LIGHT));
   }
 };
